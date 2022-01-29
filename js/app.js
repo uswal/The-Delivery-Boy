36d979e6590bddaa4edb8252b4dee7ab590c3ba2
uswal <uswalanand1999@gmail.com>
2022-01-30 02:15:18 +0530

    30-1

---
+++ js/app.js
@@ -0,0 +1,59 @@
+var menuState = "LEADERBOARD"; // MENU  TUTORIAL LEADERBOARD PLAY
+var playState = "SHEET"; // ARE_YOU_READY SHEET LEVEL HINTS RESULT
+const frame = document.getElementById("frame");
+
+function changeToDefaultMenu() {
+  menuState = "MENU";
+  frame.innerHTML = `
+            <div class="container col-flex menu">
+            <label class="header">MENU</label>
+            <label class="big-option">PLAY</label>
+            <label class="big-option">TUTORIAL</label>
+            <label class="big-option" id="leaderboard">LEADERBOARD</label>
+          </div>
+            `;
+  document
+    .getElementById("leaderboard")
+    .addEventListener("click", changeToLeaderboard);
+}
+function changeToLeaderboard() {
+  menuState = "LEADERBOARD";
+  frame.innerHTML = `
+    <div class="container col-flex leaderboard">
+      <label class="header">LEADERBOARD</label>
+      <table>
+        <tr>
+          <th>Rank</th>
+          <th>Username</th>
+          <th>High Score</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Oswald</td>
+          <td>1200Points</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Oswald</td>
+          <td>1200Points</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Oswald</td>
+          <td>1200Points</td>
+        </tr>
+      </table>
+
+      <div>
+        <button>PREVIOUS</button>
+        <button>NEXT</button>
+      </div>
+
+      <button id="main-menu">BACK TO MAIN MENU</button>
+    </div>
+    `;
+  document
+    .getElementById("main-menu")
+    .addEventListener("click", changeToDefaultMenu);
+}
+changeToDefaultMenu();
